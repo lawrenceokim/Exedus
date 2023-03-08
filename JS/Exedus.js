f6e3b98d528a2d5ae2a62fdd044d91a4b289cf60
lawrenceokim <lawrence4okim@gmail.com>
2023-03-08 15:43:01 +0100

    dashboard nav complete

--- JS/Exedus.js
+++ JS/Exedus.js
@@ -1,20 +1,33 @@
 "use-strict";
-const messagePopUp = document.querySelector(".signup-container");
+const signupPopUp = document.querySelector(".signup-container");
+const btnSignup = document.querySelector(".cta-btn");
+const btnSignup1 = document.querySelector(".signup1");
+const btnSignup2 = document.querySelector(".signup2");
+const btnSignup3 = document.querySelector(".signup3");
+
 const overlay = document.querySelector(".overlay");
 const btnClose = document.querySelector(".btn-close");
 
 // FUNCTIONS
-const hidePopUp = function () {
+const hideSignup = function () {
   overlay.classList.add("disabled");
-  messagePopUp.classList.add("disabled");
+  signupPopUp.classList.add("disabled");
+};
+const showSignup = function () {
+  overlay.classList.remove("disabled");
+  signupPopUp.classList.remove("disabled");
 };
 
+btnSignup.addEventListener("click", showSignup);
+btnSignup1.addEventListener("click", showSignup);
+btnSignup2.addEventListener("click", showSignup);
+btnSignup3.addEventListener("click", showSignup);
+btnClose.addEventListener("click", hideSignup);
+overlay.addEventListener("click", hideSignup);
 // POP-UP TIMER
 // setTimeout(function () {
 //   overlay.classList.remove("disabled");
 // }, 4600);
 // setTimeout(function () {
-//   messagePopUp.classList.remove("disabled");
+//   signupPopUp.classList.remove("disabled");
 // }, 4700);
-btnClose.addEventListener("click", hidePopUp);
-overlay.addEventListener("click", hidePopUp);
